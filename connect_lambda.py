bf17de93a35d9e3d1f6dee1ce8a97b38319c635b
Alfin Shaji <alfinshaji198@gmail.com>
2026-03-18 20:08:21 +0530

    lambda function need further mods accordingly

---
+++ connect_lambda.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import time
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+
+def lambda_handler(event, context):
+    print("Connecting")
+    
+    try:
+        connection_id = event['requestContext']['connectionId']
+        authorizer = event['requestContext'].get('authorizer', {})
+        employee_id = authorizer.get('principalId', 'unknown_user')
+        ttl_seconds = int(time.time()) + (24 * 60 * 60)
+        item = {
+            'connectionId': {'S': connection_id},
+            'employeeId': {'S': employee_id},
+            'ttl': {'N': str(ttl_seconds)}
+        }
+        
+        print(f"Saving to Connection Tables: {json.dumps(item)}")
+        return {
+            'statusCode': 200,
+            'body': 'Connected.'
+        }
+     
+    except Exception as e:
+        print(f"Connection failed: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': 'Error Connecting.'
+        }
